d2dec5c8a8ef080c5f9c59a566c2dc0a5e8632a5
Dominik Ranters <dominik.ranters@tobit.cm>
2019-08-21 17:17:57 +0200

    :truck: Crate form files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import { hot } from 'react-hot-loader';
-import Headline from './header/headline/Headline';
 import Intro from './header/intro/Intro';
+import Form from './form/Form';
 
 // We use PureComponent instead of Component because it handles the shouldComponentUpdate method for us.
 // If we want to define our own shouldComponentUpdate logic we have to use Component instead of PureComponent.
@@ -9,10 +9,12 @@ class App extends PureComponent {
     render() {
         return (
             <div className="tapp">
-                <Headline headline="Sites"/>
-                <Intro intro="Finden Sie neue Sites oder fügen Sie ihre eigene hinzu."/>
+                <Intro 
+                    intro="Finden Sie neue Sites oder fügen Sie ihre eigene hinzu."
+                    headline="Sites"
+                />
 
-                <Formular />
+                <Form />
             </div>
         );
     }
---
+++ src/components/form/Form.jsx
@@ -0,0 +1,53 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { Accordion } from 'chayns-components';
+import { Input } from 'chayns-components';
+import { Button, ChooseButton } from 'chayns-components';
+
+class Form extends PureComponent {
+    render() {
+        return (
+            <Accordion head="Hinzufügen">
+                <div className="accordion__content">
+
+                    <Input
+                        name="name"
+                        placeholder="Name*"
+                        style={{
+                            marginBottom: '10px',
+                        }}
+                    />
+
+                    <Input
+                        name="url"
+                        placeholder="Ulr*"
+                        style={{
+                            marginBottom: '10px',
+                        }}
+                    />
+
+
+                    <Input
+                        name="email"
+                        placeholder="eMail*"
+                        style={{
+                            marginBottom: '10px',
+                        }}
+                    />
+
+
+                    <Input
+                        name="kommentar"
+                        placeholder="Kommentar"
+                        style={{
+                            marginBottom: '10px',
+                        }}
+                    />
+
+                </div>
+            </Accordion>
+        )
+    }
+}
+
+export default Form
